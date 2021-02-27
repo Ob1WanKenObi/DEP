68d4c8391eafa8272d0554b31df723ae114860d2
Sparsh Agarwal <2018csb1126@iitrpr.ac.in>
2021-02-27 22:26:20 +0530

    komedi

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import BarChart from './Components/BarChart/BarChart';
 import LineChart from './Components/LineChart/LineChart';
 import DoughnutChart from './Components/DoughnoutChart/DoughnotChart';
 import SignUp from './Components/SignUp/SignUp';
-import DataEntry from './Components/DataEntry/DataEntry';
-import ShowChart from './Components/ShowChart/ShowChart';
 
 const timeline = ['2011', '2012', '2013', '2014', '2015'];
 const data = [10, 11, 23, 30, 25];
@@ -23,7 +21,7 @@ class App extends Component {
   render() {
     return (
       // <DoughnutChart timeLine={timeline} dataSet={data} legend="Death per year in millions" />
-      <ShowChart />
+      <SignUp />
     );
   }
 }
