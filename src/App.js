d3c544aca94c635239fa987ad69be878448404c5
Sparsh Agarwal <2018csb1126@iitrpr.ac.in>
2021-02-28 02:43:22 +0530

    Made some small changes to remove compilation warnings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
-import BarChart from './Components/BarChart/BarChart';
-import LineChart from './Components/LineChart/LineChart';
-import DoughnutChart from './Components/DoughnoutChart/DoughnotChart';
 import SignUp from './Components/SignUp/SignUp';
-import DataEntry from './Components/DataEntry/DataEntry';
 import ShowChart from './Components/ShowChart/ShowChart';
 
 
-const timeline = ['2011', '2012', '2013', '2014', '2015'];
-const data = [10, 11, 23, 30, 25];
-
 class App extends Component {
-  state = {
-    timeline: null,
-    data: null,
-    legend: '',
-    showChart: true,
-  }
-
-
-
 
   render() {
     return (
