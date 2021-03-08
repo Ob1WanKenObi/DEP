9417e1a51925fbbeb4fb57022930ac6ab20eb6da
Daksh Sharma <68499451+2018csb1082@users.noreply.github.com>
2021-03-08 21:26:17 +0530

    css styling on forms
    lol

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ class App extends Component {
     return (
       // <DoughnutChart timeLine={timeline} dataSet={data} legend="Death per year in millions" />
       //<BarChart timeLine ={timeline} dataSet={data} legend="death in millions per months" />
-      <ShowChart />
-      //<SignUp />
+      //<ShowChart />
+      <div className="container">
+        <h1 class = "major-heading-center">SIGN UP!</h1>
+        <SignUp />
+      </div>
+      /*<div className="container">
+        <ShowChart />
+      </div>*/
     );
   }
 }
