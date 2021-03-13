563eed8b640138b825626fbb3861e2f9678182fd
Daksh Sharma <68499451+2018csb1082@users.noreply.github.com>
2021-03-13 19:27:34 +0530

    home page revamped
    done some shadow play

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import covid from '../../common/images/corona.png';
 import agri from '../../common/images/agri.png';
+import tax from '../../common/images/tax.jpg';
 
 class HomePage extends Component {
 
@@ -10,13 +11,16 @@ class HomePage extends Component {
             <div class="grid-container">
                 <div class="grid-item">
                     Covid Data<br></br>
-                    <img src={covid} alt="COVID" style={{width:"180px",height:"180px"}}></img>
+                    <img src={covid} alt="COVID" style={{width:"180px",height:"180px",marginTop:"10px"}}></img>
                 </div>
                 <div class="grid-item">
                     Agricultural Data<br></br>
-                    <img src={agri} alt="COVID" style={{width:"180px",height:"180px"}}></img>
+                    <img src={agri} alt="COVID" style={{width:"180px",height:"180px",marginTop:"10px"}}></img>
                 </div>
-                <div class="grid-item">Samlpe-heading-3</div>
+                <div class="grid-item">
+                    Tax Data<br></br>
+                    <img src={tax} alt="COVID" style={{width:"180px",height:"180px",marginTop:"10px"}}></img>
+                </div>
                 <div class="grid-item">Samlpe-heading-4</div>
                 <div class="grid-item">Samlpe-heading-5</div>
                 <div class="grid-item">Samlpe-heading-6</div>
