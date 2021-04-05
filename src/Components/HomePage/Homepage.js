5e82de2c65e60a58351849ddf009bd423748c0ae
Daksh Sharma <68499451+2018csb1082@users.noreply.github.com>
2021-04-05 16:56:38 +0530

    restructured HomePage
    better design, still lacks image-button-routing

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
-import covid from '../../common/images/corona.png';
-import agri from '../../common/images/agri.png';
-import tax from '../../common/images/tax.jpg';
+import admin from '../../common/images/admin.png';
+import dev from '../../common/images/dev.png';
+import seasonal from '../../common/images/seasonal.png';
+import structure from '../../common/images/structure.png';
 
 class HomePage extends Component {
 
 
     render() {
         return (
-            <div class="grid-container">
-                <div class="grid-item">
-                    Covid Data<br></br>
-                    <img src={covid} alt="COVID" style={{ width: "180px", height: "180px", marginTop: "10px" }}></img>
+            <div class="grid-container-home" style={{gridTemplateColumns: "2fr 2fr"}}>
+                <div class="grid-item-home" style={{ backgroundColor: "#b5f5f4"}}>
+                    <div>General Administration</div>
+                    <img src={admin} alt="General Administration" style={{ width: "180px", height: "180px", marginTop: "10px" }}></img>
                 </div>
-                <div class="grid-item">
-                    Agricultural Data<br></br>
-                    <img src={agri} alt="COVID" style={{ width: "180px", height: "180px", marginTop: "10px" }}></img>
+                <div class="grid-item-home" style={{ backgroundColor: "#d2b6fa"}}>
+                    <div>Development Administration</div>
+                    <img src={dev} alt="Development Administration" style={{ width: "220px", height: "180px", marginTop: "10px" }}></img>
                 </div>
-                <div class="grid-item">
-                    Tax Data<br></br>
-                    <img src={tax} alt="COVID" style={{ width: "180px", height: "180px", marginTop: "10px" }}></img>
+                <div class="grid-item-home" style={{ backgroundColor: "#d2b6fa"}}> 
+                    <div>Seasonal Work</div>
+                    <img src={seasonal} alt="Seasonal work" style={{ width: "180px", height: "180px", marginTop: "10px" }}></img>
                 </div>
-                <div class="grid-item">sample-heading-4</div>
-                <div class="grid-item">sample-heading-5</div>
-                <div class="grid-item">sample-heading-6</div>
+                <div class="grid-item-home" style={{ backgroundColor: "#b5f5f4"}}>
+                    <div>Structured Drivers and Working</div>
+                    <img src={structure} alt="Structured Drivers and Working" style={{ width: "180px", height: "180px", marginTop: "10px" }}></img>
+                </div>
             </div>
         );
     }
