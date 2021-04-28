1aa9a0772682186d42ff4e5a6978ff84e309f10f
Sparsh Agarwal <2018csb1126@iitrpr.ac.in>
2021-04-28 05:31:06 +0530

    Search Query API hit added

--- src/Components/RevenuePage/RevenuePage.js
+++ src/Components/RevenuePage/RevenuePage.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
 import SearchBar from '../SearchBar/SearchBar';
-import AllChart from '../AllChart/AllChart';
 import alert from '../../common/images/alert.svg';
 import rupee from '../../common/images/currency-inr.svg';
 import MultiChart from '../MultiCharts/MultiCharts';
 
 const timeLine = ['January', 'February', 'March', 'April', 'May'];
-const dataset =    [[3311, 18344, 10334, 19443, 15224],
-                    [8311, 17344, 12334, 17443, 19224],
-                    [13311, 16344, 14334, 15443, 11224],
-                    [18311, 15344, 14334, 15443, 15224],
-                    [23311, 14344, 12334, 17443, 17224],
-                    [28311, 13344, 10334, 19443, 13224]];
+const dataset = [[3311, 18344, 10334, 19443, 15224],
+[8311, 17344, 12334, 17443, 19224],
+[13311, 16344, 14334, 15443, 11224],
+[18311, 15344, 14334, 15443, 15224],
+[23311, 14344, 12334, 17443, 17224],
+[28311, 13344, 10334, 19443, 13224]];
 const dataSource = [
   {
     key: '1',
@@ -65,89 +64,114 @@ const dataSource = [
   },
 ];
 const columns = [
-    {
-      title: 'Month',
-      dataIndex: 'Month',
-      fixed: 'left',
-    },
-    {
-      title: 'Data1',
-      dataIndex: 'd1',
-      sorter: (a, b) => a.d1 - b.d1,
-      sortDirections: ['descend', 'ascend'],
-    },
-    {
-      title: 'Data2',
-      dataIndex: 'd2',
-      sorter: (a, b) => a.d2 - b.d2,
-      sortDirections: ['descend', 'ascend'],
-    },
-    {
-      title: 'Data3',
-      dataIndex: 'd3',
-      sorter: (a, b) => a.d3 - b.d3,
-      sortDirections: ['descend', 'ascend'],
-    },
-    {
-      title: 'Data4',
-      dataIndex: 'd4',
-      sorter: (a, b) => a.d4 - b.d4,
-      sortDirections: ['descend', 'ascend'],
-    },
-    {
-      title: 'Data5',
-      dataIndex: 'd5',
-      sorter: (a, b) => a.d5 - b.d5,
-      sortDirections: ['descend', 'ascend'],
-    },
-  ];
+  {
+    title: 'Month',
+    dataIndex: 'Month',
+    fixed: 'left',
+  },
+  {
+    title: 'Data1',
+    dataIndex: 'd1',
+    sorter: (a, b) => a.d1 - b.d1,
+    sortDirections: ['descend', 'ascend'],
+  },
+  {
+    title: 'Data2',
+    dataIndex: 'd2',
+    sorter: (a, b) => a.d2 - b.d2,
+    sortDirections: ['descend', 'ascend'],
+  },
+  {
+    title: 'Data3',
+    dataIndex: 'd3',
+    sorter: (a, b) => a.d3 - b.d3,
+    sortDirections: ['descend', 'ascend'],
+  },
+  {
+    title: 'Data4',
+    dataIndex: 'd4',
+    sorter: (a, b) => a.d4 - b.d4,
+    sortDirections: ['descend', 'ascend'],
+  },
+  {
+    title: 'Data5',
+    dataIndex: 'd5',
+    sorter: (a, b) => a.d5 - b.d5,
+    sortDirections: ['descend', 'ascend'],
+  },
+];
+
+const DataType = [
+  {
+    tableName: 'excise_duty_tax',
+    name: 'Collection of Excise',
+  },
+  {
+    tableName: 'gst',
+    name: 'Collection of GST',
+  },
+  {
+    tableName: 'motor_vehicle_tax',
+    name: 'Collection of Motor Vehicle Tax',
+  },
+  {
+    tableName: 'stamp_duty',
+    name: 'Collection of Stamp Duty',
+  }
+];
 
 const RevenuePage = () => {
-    const DataType = ['Collection of Excise', 'Collection of GST', 'Collection of Motor Vehicle Tax', 'Collection of Stamp Duty'];
-    const [showStats, setShowStats] = useState(true);
-    const [typeofData, setTypeOfData] = useState(null);
+  const [showStats, setShowStats] = useState(true);
+  const [typeofData, setTypeOfData] = useState(null);
+  const [fetchedData, setFetchedData] = useState(null);
 
-    const showStatsHandler = () => {
-        setShowStats(false);
-    }
+  const showStatsHandler = () => {
+    setShowStats(false);
+  }
 
-    const dataTypeHandler = (datatype) => {
-        setTypeOfData(datatype);
-    }
+  const fetchedDataHandler = (data) => {
+    setFetchedData(data)
+  }
 
-    let statistics =
-        <div className="grid-container-datasearch-2" style={{ gridTemplateColumns: "2fr 2fr" }}>
-            <div className="grid-item-datasearch-2" style={{ backgroundColor: "#1cb077" }}>
-                <div className="search-data-heading" >Revenue Collection in 2020</div>
-                <div className="search-data-data"><span><img src={rupee} className="mx-2" />20,18,30,670</span></div>
-            </div>
-            <div className="grid-item-datasearch-2" style={{ backgroundColor: "#f78820" }}>
-                <div className="search-data-heading">Revenue Collection in 2021</div>
-                <div className="search-data-data"><span><img src={rupee} className="mx-2" />5,46,68,640</span></div>
-            </div>
-            <div className="grid-item-datasearch-2" style={{ backgroundColor: "#e3685d" }}>
-                <div className="search-data-heading">Expected Collection in 2021</div>
-                <div className="search-data-data"><span><img src={rupee} className="mx-2" />21,02,64,000</span></div>
-            </div>
-            <div className="grid-item-datasearch-2" style={{ backgroundColor: "#7aba20" }}>
-                <div className="search-data-heading">Percentage collected by far</div>
-                <div className="search-data-data"><span><img src={alert} className="mx-2" />26%</span></div>
-            </div>
-        </div>
+  const dataTypeHandler = (datatype, data) => {
+    const getDataTypeName = DataType.filter(obj => obj.tableName === datatype)[0].name;
+    setTypeOfData(getDataTypeName);
+    fetchedDataHandler(data);
+  }
+
+  let statistics =
+    <div className="grid-container-datasearch-2" style={{ gridTemplateColumns: "2fr 2fr" }}>
+      <div className="grid-item-datasearch-2" style={{ backgroundColor: "#1cb077" }}>
+        <div className="search-data-heading" >Revenue Collection in 2020</div>
+        <div className="search-data-data"><span><img src={rupee} className="mx-2" />20,18,30,670</span></div>
+      </div>
+      <div className="grid-item-datasearch-2" style={{ backgroundColor: "#f78820" }}>
+        <div className="search-data-heading">Revenue Collection in 2021</div>
+        <div className="search-data-data"><span><img src={rupee} className="mx-2" />5,46,68,640</span></div>
+      </div>
+      <div className="grid-item-datasearch-2" style={{ backgroundColor: "#e3685d" }}>
+        <div className="search-data-heading">Expected Collection in 2021</div>
+        <div className="search-data-data"><span><img src={rupee} className="mx-2" />21,02,64,000</span></div>
+      </div>
+      <div className="grid-item-datasearch-2" style={{ backgroundColor: "#7aba20" }}>
+        <div className="search-data-heading">Percentage collected by far</div>
+        <div className="search-data-data"><span><img src={alert} className="mx-2" />26%</span></div>
+      </div>
+    </div>
 
 
 
-    return (
-        <div>
-            <div><SearchBar Datatypes={DataType} statsoffaHandler={showStatsHandler} typeOfDataHandler={dataTypeHandler} /></div>
-            {showStats ? statistics : 
-            <div className="chart-box">
-                <div>
-                    <MultiChart timeline={timeLine} datasets={dataset} legend={`${typeofData} per month`} dataSource={dataSource} columns={columns}/>
-                </div>
-            </div>}
-        </div>
-    )
+  return (
+    <div>
+      <div><SearchBar Datatypes={DataType} statsoffaHandler={showStatsHandler} typeOfDataHandler={dataTypeHandler} /></div>
+      {showStats ? statistics :
+        <div className="chart-box">
+          <div>
+            <MultiChart timeline={timeLine} datasets={dataset} legend={`${typeofData} per month`} dataSource={dataSource} columns={columns} />
+          </div>
+        </div>}
+    </div>
+  )
 }
 
 export default RevenuePage;
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Formik, ErrorMessage, Field, Form } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const yearList = ['2021', '2020'];
-const districts = ["Ropar", "d1", "d2", "d3", "d4", "d5", "d6", "d7"];
+const districts = ["Ropar", "chd", "d2", "d3", "d4", "d5", "d6", "d7"];
+
+const API = {
+    URL: 'http://127.0.0.1:8000/',
+}
 
 const SearchBar = ({ Datatypes, statsoffaHandler, typeOfDataHandler }) => {
     const initialValues = {
-        DataType: Datatypes[0],
-        FromMonth: monthList[0],
+        DataType: Datatypes[0].tableName,
+        FromMonth: 1,
         FromYear: yearList[0],
-        ToMonth: monthList[0],
+        ToMonth: 1,
         ToYear: yearList[0],
     }
 
@@ -23,17 +28,24 @@ const SearchBar = ({ Datatypes, statsoffaHandler, typeOfDataHandler }) => {
             FromYear: values.FromYear,
             ToMonth: values.ToMonth,
             ToYear: values.ToYear,
+            district: values.District,
         }
-        typeOfDataHandler(data.DataType);
-        console.log(values);
+        axios.get(`${API.URL}customquery/?start_year=${data.FromYear}&start_month=${data.FromMonth}&end_year=${data.ToYear}&end_month=${data.ToMonth}&district=${data.district}&tablename=${data.DataType}`)
+            .then(response => {
+                console.log(response);
+                typeOfDataHandler(data.DataType, response.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     const validationSchema = Yup.object({
-        DataType: Yup.string().required('Required'),
-        FromMonth: Yup.string().required('Required'),
-        FromYear: Yup.string().required('Required'),
-        ToMonth: Yup.string().required('Required'),
-        ToYear: Yup.string().required('Required'),
+        DataType: Yup.string().required('Required !'),
+        FromMonth: Yup.string().required('Required !'),
+        FromYear: Yup.string().required('Required !'),
+        ToMonth: Yup.string().required('Required !'),
+        ToYear: Yup.string().required('Required !'),
     });
 
     return (
@@ -43,7 +55,7 @@ const SearchBar = ({ Datatypes, statsoffaHandler, typeOfDataHandler }) => {
                 onSubmit={(values) => formSubmitHandler(values, typeOfDataHandler)}
                 validationSchema={validationSchema}>
                 <div>
-                    <div className="grid-container-search" style={{}}>
+                    <div className="grid-container-search">
                         <div className="grid-item-search d-flex justify-content-center" >
                             <div style={{ width: "100%" }}>
                                 Data Type
@@ -80,8 +92,8 @@ const SearchBar = ({ Datatypes, statsoffaHandler, typeOfDataHandler }) => {
                             <div className="grid-item-search d-flex justify-content-center" >
                                 <div style={{ width: "100%" }}>
                                     <Field as="select" name="DataType" className="input-area-1">
-                                        {Datatypes.map(datatype =>
-                                            <option key={datatype} value={datatype}>{datatype}</option>)}
+                                        {Datatypes.map(({ name, tableName }) =>
+                                            <option key={name} value={tableName}>{name}</option>)}
                                     </Field>
                                     <ErrorMessage name="DataType" />
                                 </div>
@@ -98,8 +110,8 @@ const SearchBar = ({ Datatypes, statsoffaHandler, typeOfDataHandler }) => {
                             <div className="grid-item-search d-flex justify-content-center" >
                                 <div style={{ width: "100%" }}>
                                     <Field as="select" name="FromMonth" className="input-area-1" style={{ width: "45%", display: "inline", marginRight: "5%" }}>
-                                        {monthList.map(month =>
-                                            <option key={month} value={month}>{month}</option>)}
+                                        {monthList.map((month, index) =>
+                                            <option key={month} value={index + 1}>{month}</option>)}
                                     </Field>
 
                                     <Field as="select" name="FromYear" className="input-area-1" style={{ width: "45%", display: "inline", marginLeft: "5%" }}>
@@ -112,8 +124,8 @@ const SearchBar = ({ Datatypes, statsoffaHandler, typeOfDataHandler }) => {
                             <div className="grid-item-search d-flex justify-content-center" >
                                 <div style={{ width: "100%" }}>
                                     <Field as="select" name="ToMonth" className="input-area-1" style={{ width: "45%", display: "inline", marginRight: "5%" }}>
-                                        {monthList.map(month =>
-                                            <option key={month} value={month}>{month}</option>)}
+                                        {monthList.map((month, index) =>
+                                            <option key={month} value={index + 1}>{month}</option>)}
                                     </Field>
 
                                     <Field as="select" name="ToYear" className="input-area-1" style={{ width: "45%", display: "inline", marginLeft: "5%" }}>
