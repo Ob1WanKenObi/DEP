547c953e38f6fe6a100d04977cc98e7b91501203
Daksh Sharma <68499451+2018csb1082@users.noreply.github.com>
2021-05-20 01:51:14 +0530

    some work

--- src/Components/CoronaVirus/CoronaVirus.js
+++ src/Components/CoronaVirus/CoronaVirus.js
@@ -3,6 +3,10 @@ import rupee from '../../common/images/currency-inr.svg';
 import Positivity from '../positivity/positivity';
 import ContactTracing from '../ContactTracing/ContactTracing';
 
+import allergy from '../../common/images/allergy.svg';
+import plusth from '../../common/images/plus-thick.svg';
+import skull from '../../common/images/skull-outline.svg';
+
 const { TabPane } = Tabs;
 
 
@@ -20,17 +24,17 @@ export const CoronaVirus = () => {
         <div>
             <div style={{ backgroundColor: "white" }}>
                 <Tabs defaultActiveKey="1" onChange={changeHandler} type="card" className="major-upper-tabs">
-                    <TabPane key="1" tab={<StyledTab>Contact Tracing</StyledTab>}>
+                    <TabPane key="1" tab={<StyledTab><img src={allergy} className="mx-2" />Contact Tracing</StyledTab>}>
                         <div>
                             <ContactTracing />
                         </div>
                     </TabPane>
-                    <TabPane key="2" tab={<StyledTab>Covid Positivity</StyledTab>}>
+                    <TabPane key="2" tab={<StyledTab><img src={plusth} className="mx-2" />Covid Positivity</StyledTab>}>
                         <div>
                             <Positivity />
                         </div>
                     </TabPane>
-                    <TabPane key="3" tab={<StyledTab>Deaths</StyledTab>}>
+                    <TabPane key="3" tab={<StyledTab><img src={skull} className="mx-2" />Deaths</StyledTab>}>
                         <div>
                             <ContactTracing />
                         </div>
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -7,6 +7,7 @@ import { Redirect, Link } from 'react-router-dom';
 import LogIn from '../LogIn/Login';
 import bgg from '../../common/images/signin.svg';
 import { LoginOutlined } from '@ant-design/icons';
+import Header from '../Header/index';
 
 const initialValues = {
     name: '',
@@ -66,13 +67,16 @@ const SignUp = () => {
 
 
     return (
-        isSignedUp === true ? <Redirect to="/" /> : (<Formik
+        isSignedUp === true ? <Redirect to="/" /> : (
+        <div>
+        <Header />
+        <Formik
             initialValues={initialValues}
             onSubmit={formSubmitHandler}
             validationSchema={validationSchema}>
             <Form>
-            <div className="login-main" style={{background: `linear-gradient(45deg, #b5ffdb 0%, #ffffff 50%,#ffd1a6 100%)`}}> {/*`url(${bgg}) no-repeat center center fixed`*/}
-                        <div style={{padding: "3%", border: "2px solid black",borderRadius: "10px", width: "40%", marginLeft: "30%", marginTop: "0px", boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)" }}>
+            <div className="login-main" style={{background: `url(${bgg}) no-repeat center center fixed`}}> {/*`linear-gradient(45deg, #b5ffdb 0%, #ffffff 50%,#ffd1a6 100%)`*/}
+                        <div style={{padding: "3%", border: "2px solid black",borderRadius: "10px", width: "40%", marginLeft: "33%", marginTop: "0px", boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)" }}>
                             <h2 className="minor-heading-3">Sign Up to use the website</h2>
 
                             <div>
@@ -145,7 +149,8 @@ const SignUp = () => {
                     </div>
                 </div>
             </Form>
-        </Formik >)
+        </Formik >
+        </div>)
     );
 }
 
